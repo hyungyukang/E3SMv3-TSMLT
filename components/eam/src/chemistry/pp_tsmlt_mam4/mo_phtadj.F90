968d1104bbe9e3b1a89182edd824457cefefd5bb
Hyun Kang <kangh@ornl.gov>
2023-12-04 03:03:34 -0500

    Change dimension in tsmlt_mam4 and add definition

--- components/eam/src/chemistry/pp_tsmlt_mam4/mo_phtadj.F90
+++ components/eam/src/chemistry/pp_tsmlt_mam4/mo_phtadj.F90
@@ -2,23 +2,24 @@
       private
       public :: phtadj
       contains
-      subroutine phtadj( p_rate, inv, m, ncol, nlev )
+      subroutine phtadj( p_rate, inv, m, ncol)
       use chem_mods, only : nfs, phtcnt
       use shr_kind_mod, only : r8 => shr_kind_r8
+      use ppgrid,       only : pver
       implicit none
 !--------------------------------------------------------------------
 ! ... dummy arguments
 !--------------------------------------------------------------------
-      integer, intent(in) :: ncol, nlev
-      real(r8), intent(in) :: inv(ncol,nlev,max(1,nfs))
-      real(r8), intent(in) :: m(ncol,nlev)
-      real(r8), intent(inout) :: p_rate(ncol,nlev,max(1,phtcnt))
+      integer, intent(in) :: ncol
+      real(r8), intent(in) :: inv(ncol,pver,max(1,nfs))
+      real(r8), intent(in) :: m(ncol,pver)
+      real(r8), intent(inout) :: p_rate(ncol,pver,max(1,phtcnt))
 !--------------------------------------------------------------------
 ! ... local variables
 !--------------------------------------------------------------------
       integer :: k
-      real(r8) :: im(ncol,nlev)
-      do k = 1,nlev
+      real(r8) :: im(ncol,pver)
+      do k = 1,pver
          im(:ncol,k) = 1._r8 / m(:ncol,k)
          p_rate(:,k,112) = p_rate(:,k,112) * inv(:,k, 2) * im(:,k)
          p_rate(:,k,113) = p_rate(:,k,113) * inv(:,k, 2) * im(:,k)
